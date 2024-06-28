010c98344f7e0378506007e7699669e65d4323a7
thilaganiranchan <135204177+thilaganiranchan@users.noreply.github.com>
2024-06-28 13:14:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnElList =document.querySelectorAll(".apply");
+
+// btnElList.forEach(btnEl=>{
+//     btnEl.addEventListener('click',()=>{
+//         document.querySelector('.tick').classList.remove('tick');
+//         btnEl.classList.add('tick');
+//         btnElList.textcontent='<span class="tick">&#10003;</span>';
+//     })
+// })
+
+
+function check()
+{
+    btnElList.textcontent="ok";
+}
